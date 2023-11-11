4a3e44cdd96af2c4d4dc04535ff6202d59c1432a
SoulsTogetherX <xalvarez4444@gmail.com>
2023-11-10 21:59:06 -0800

    can now pick up one segment at once

--- src/player/player.gd
+++ src/player/player.gd
@@ -13,19 +13,22 @@ const GRAVITY             : int    =  1960 / 2;
 @export var _cam           : CameraFollow = null;
 var _tween                 : Tween;
 var _jumping               : bool = false;
-var _holding               : Array[Segment];
+var _selecting             : Array[Segment];
+var _held                  : Segment;
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact") && _holding.is_empty():
+	if event.is_action_pressed("interact") && _selecting.is_empty():
 		var detecteds = detector.get_overlapping_areas();
 		for detected in detecteds:
 			var subject = detected.owner;
 			if subject is Segment && is_on_floor() && !subject.freeze:
-				_holding.append(subject);
-				subject.drop();
-				state_contr.force_change_state("selecting", 0);
+				_selecting.append(subject);
 			else:
 				subject.action();
+		if !_selecting.is_empty():
+			if _held:
+				_selecting.append(_held);
+			state_contr.force_change_state("selecting", 0);
 
 func _physics_process(delta) -> void:	
 	pass;
--- src/state_machine/states/selecting.gd
+++ src/state_machine/states/selecting.gd
@@ -2,24 +2,34 @@ extends State
 
 @export var idle : State;
 
-const HOLD_OFFSET = Vector2(0, 30);
+const HOLD_OFFSET : Vector2 = Vector2(0, -30);
+
+var select_idx  : int;
+var max_selects : int;
 
 func state_name():
 	return "selecting";
 
 func enter() -> void:
-	for held in actor._holding:
+	for held in actor._selecting:
 		held.hold();
-		GlobalStuff.swap_parent(held, self);
+		GlobalStuff.swap_parent(held, owner);
+	max_selects = actor._selecting.size() - 1;
+	select_idx  = max_selects;
+	await organize_start();
 
 func exit() -> void:
-	for held in actor._holding:
+	for held in actor._selecting:
 		held.drop();
-	actor._holding.clear();
+	actor._selecting.clear();
 
 func process_input(event: InputEvent) -> State:
-	if Input.is_action_just_pressed("jump"):
+	if event.is_action_pressed("jump"):
 		return idle;
+	var dir : int = int(event.is_action_pressed("right")) - int(event.is_action_pressed("left"));
+	_change_index(dir);
+	await organize_selections(dir);
+	
 	return null;
 
 func process_frame(_delta: float) -> State:
@@ -28,3 +38,31 @@ func process_frame(_delta: float) -> State:
 func process_physics(_delta: float) -> State:
 	actor._cam.update_position();
 	return null;
+
+func organize_start():
+	var tw : Tween = create_tween();
+	match max_selects:
+		0:
+			tw.tween_property(actor._selecting[0], "position", HOLD_OFFSET, 0.2);
+		1:
+			pass;
+		_:
+			pass;
+	await tw.finished;
+
+func _change_index(dir : int):
+	select_idx += dir;
+	if select_idx < 0:
+		select_idx = max_selects;
+	elif select_idx > max_selects:
+		select_idx = 0;
+
+func organize_selections(dir : int):
+	match max_selects:
+		0:
+			
+			pass;
+		1:
+			pass;
+		_:
+			pass;
